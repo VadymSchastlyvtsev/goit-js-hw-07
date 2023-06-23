7ba02f08a4c6bc34ca627bf756913129ea790868
VadymSchastlyvtsev <drdre6870@gmail.com>
2023-06-23 19:59:15 +0300

    added murcup
    added murcup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const createGalleryMarkup = items => {
+  return items
+    .map(item => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            data-source="${item.original}"
+          />
+        </a>
+      </li>
+    `)
+    .join('');
+};
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+gallery.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+    evt.preventDefault();
+
+    const element = evt.target;
+    if (element.classList.contains('gallery__image')) {
+        const id = element.dataset.source;
+        const instance = basicLightbox.create(`<img
+        src="${id}"
+        alt="${element.alt}"
+      />`);
+      
+
+      const handleKeyDown = event => {
+        if (event.key === 'Escape') {
+          instance.close();
+        }
+      };
+      instance.show();
+      document.addEventListener('keydown', handleKeyDown);
+    }
+};
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGalleryMarkup = items => {
+  return items
+    .map(item => `
+    <li class="item.gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>
+    `)
+    .join('');
+};
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
